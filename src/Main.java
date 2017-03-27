48d6c4ad8eb6a7e2222cc00a488b2e8c678ee00c
stevensaito <saito.tan14@gmail.com>
2017-03-26 23:59:11 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -6,11 +7,174 @@ import javax.swing.border.EmptyBorder;
 import java.io.*; // For Input Stream
 
 
-public class Main {
+public class Main extends JFrame {
+
+    //Frame width and height
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 800;
 	
-	static JTextField name;
-    static JTextField upload;
+	//Objects
+	private JPanel TitlePanel, UploadPanel, ButtonPanel, InstructionPanel, NoisePanel, ScalePanel;
+	private JLabel TitleLabel, InstructionLabel;
+	private JButton ExitButton, UpscaleButton;
+	private JRadioButton NoiseNoneRadio, NoiseLowRadio, NoiseMediumRadio, NoiseHighRadio;
+	private JRadioButton OneRadio, OneSixRadio, TwoTimesRadio;
+	private JTextArea theText; //TEMPORARY MIDDLE!!!!!!!!!!!!
+	
+	
+    //GUI interface Constructor-----------------------------
+    public Main()
+    {
+    	super("Main");
+    	setTitle("Waif2x"); //set title of window
+    	setSize(WIDTH, HEIGHT); //set size of window
+    	setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+    	
+    	BuildTitlePanel();
+    	BuildInstructionPanel();
+    	BuildUploadPanel();
+    	BuildNoisePanel();
+    	BuildScalePanel();
+    	BuildButtons();
+    	
+    	setLayout(new GridLayout(6, 1)); //set gridlayout
+    	add(TitlePanel);
+    	add(InstructionPanel);
+    	add(UploadPanel);
+    	add(NoisePanel);
+    	add(ScalePanel);
+    	add(ButtonPanel);
+    }
     
+    
+	public void BuildTitlePanel()
+	{
+		TitleLabel = new JLabel("Waifu 2x Upscaling Program");
+		
+		//create TitlePanel
+		TitlePanel = new JPanel();
+		
+		//add labels to the panel
+		TitlePanel.add(TitleLabel);
+	}
+	
+	public void BuildInstructionPanel()
+	{
+		InstructionLabel = new JLabel("Upload your image to the by dragging and dropping");
+		
+		//create TitlePanel
+		InstructionPanel = new JPanel();
+		
+		//add labels to the panel
+		InstructionPanel.add(InstructionLabel);
+	}
+	
+	public void BuildUploadPanel()
+	{	
+		UploadPanel = new JPanel();
+		UploadPanel.setBackground(Color.blue);
+		theText = new JTextArea(13, 45);
+		theText.setBackground(Color.white);
+		
+		//create UploadPanel
+		UploadPanel = new JPanel();
+		
+		UploadPanel.add(theText);
+	}
+	
+	
+	public void BuildNoisePanel()
+	{
+		JLabel RadioLabel = new JLabel("Select noise reduction: ");
+		NoiseNoneRadio = new JRadioButton("None");
+		NoiseLowRadio = new JRadioButton("Low");
+		NoiseMediumRadio = new JRadioButton("Medium");
+		NoiseHighRadio = new JRadioButton("High");
+		
+		//ExitButton.addActionListener(new ExitButtonListener());
+		
+		//create ButtonPanel
+		NoisePanel = new JPanel();
+		
+		//Button group so only one can be selected
+		ButtonGroup noisegroup = new ButtonGroup();
+		noisegroup.add(NoiseNoneRadio);
+		noisegroup.add(NoiseLowRadio);
+		noisegroup.add(NoiseMediumRadio);
+		noisegroup.add(NoiseHighRadio);
+				
+		//adds label and radio buttons
+		NoisePanel.add(NoiseNoneRadio);
+		NoisePanel.add(NoiseLowRadio);
+		NoisePanel.add(NoiseMediumRadio);
+		NoisePanel.add(NoiseHighRadio);
+	}
+	
+	
+	
+	public void BuildScalePanel()
+	{
+		JLabel RadioLabel = new JLabel("Select upscale multiplier: ");
+		OneRadio = new JRadioButton("None");
+		OneSixRadio = new JRadioButton("1.6x");
+		TwoTimesRadio = new JRadioButton("2x");
+		
+		//ExitButton.addActionListener(new ExitButtonListener());
+		
+		//create ButtonPanel
+		ScalePanel = new JPanel();
+		
+		//Button group so only one can be selected
+		ButtonGroup scalegroup = new ButtonGroup();
+		scalegroup.add(OneRadio);
+		scalegroup.add(OneSixRadio);
+		scalegroup.add(TwoTimesRadio);
+				
+		//adds label and radio buttons
+		ScalePanel.add(OneRadio);
+		ScalePanel.add(OneSixRadio);
+		ScalePanel.add(TwoTimesRadio);
+	}
+	
+	public void BuildButtons()
+	{
+		ExitButton = new JButton("Exit");
+		UpscaleButton = new JButton("Upscale");
+		
+		ExitButton.addActionListener(new ExitButtonListener());
+		UpscaleButton.addActionListener(new UpscaleButtonListener());
+		
+		//create ButtonPanel
+		ButtonPanel = new JPanel();
+				
+		ButtonPanel.add(ExitButton);
+		ButtonPanel.add(UpscaleButton);
+	}
+
+	
+	
+	//-------------------BUTTONS-------------------------
+	
+	//CLOSE BUTTON----------
+	private class ExitButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			System.exit(0);
+		}
+	}
+	
+	//UPSCALE BUTTON---------
+	private class UpscaleButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			System.out.println("\nYou've pressed the 'Upscale' Button");
+		}
+	}
+	
+	//-------------------BUTTONS-------------------------
+	
 
     public static void main(String[] args) {
 
@@ -42,69 +206,11 @@ public class Main {
             System.out.println("IO Error");
         }
 
-        // GUI
+        // GUI------------------------------------------------------
+        Main GUIinterface = new Main();
+        GUIinterface.setVisible(true);
 
-        javax.swing.SwingUtilities.invokeLater(new Runnable()
-    	{
-            public void run()
-            {
-                frame();
-            }
-    	});
     }
     
-
     
-    private static void frame()
-    {
-    	
-    	//create frame
-    	JFrame frame = new JFrame("Waifu2x");
-    	name = new JTextField(10);
-        upload = new JTextField(10);
-        
-    	//create panel
-    	JPanel panel = new JPanel(new BorderLayout(5,5));
-        panel.setBorder(new EmptyBorder(10,10,10,10));
-        frame.setContentPane(panel);
-        
-    	//boundaries
-    	frame.setSize(500, 500);
-    	
-    	//Close Button
-    	JButton buttonclose = new JButton();
-    	buttonclose.setText("Exit");
-    	
-    	//When frame closes
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-        //------------
-    	JPanel namelabel = new JPanel(new GridLayout(0,1));
-        JPanel uploadlabel = new JPanel(new GridLayout(0,1));
-        panel.add(buttonclose, BorderLayout.SOUTH);
-        panel.add(namelabel, BorderLayout.WEST);
-        panel.add(uploadlabel, BorderLayout.CENTER);
-
-        //----------------
-        namelabel.add(new JLabel("Enter your name: "));
-        uploadlabel.add(name);
-        namelabel.add(new JLabel("Upload: "));
-        uploadlabel.add(upload);
-        //close = new JButton("Submit");
-
-        //panel.add(submit, BorderLayout.SOUTH);
-        frame.pack();
-        frame.setVisible(true);
-              
-
-    	//close window when button pressed
-    	buttonclose.addActionListener(new ActionListener() {
-    	    public void actionPerformed(ActionEvent e)
-    	    {
-    	       System.exit(0);
-    	    }
-    	});
-    
-    }
-
 }
